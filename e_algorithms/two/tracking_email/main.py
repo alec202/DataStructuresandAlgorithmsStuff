065e3f613cbad297907eaa214048d3f52c48d867
hamshaw <143452596+hamshaw@users.noreply.github.com>
2024-02-05 17:45:40 -0500

    final commit, full credit main.py

--- e_algorithms/two/tracking_email/main.py
+++ e_algorithms/two/tracking_email/main.py
@@ -1,16 +1,26 @@
-stack = {}
-entry = 0
-emails = int(input())
-for i in range(emails):
-    new = input()
-    if new[0] == '1':
-        level = int(new.split()[1])
-        entry += 1
-        stack[entry] = level
+import heapq
+def main():
+    heap = []
+    #heapq.heapify(heap)
+    stack = {}
+    entry = 0
+    emails = int(input())
+    for i in range(emails):
+        new = input()
+        if new[0] == '1':
+            level = int(new.split()[1])
+            entry += 1
+            stack[entry] = level
+            heapq.heappush(heap, -1*level)
 
-    elif new[0] == '2':
-        entry -= 1
+        elif new[0] == '2':
+            heap.remove(-1*(stack[entry]))
+            heapq.heapify(heap)
+            stack[entry] = 0
+            entry -= 1
 
-    elif new[0] == '3':
-        print(max(stack.values()))
+        elif new[0] == '3':
+            print(-1*heap[0])
 
+if __name__ == "__main__":
+    main()
