790dbc6f7df1e081bdc783240293346f2692a246
Alec Mirambeau <alecmirambeau@gmail.com>
2024-02-23 12:02:44 -0500

    Added exact change for example to look at

---
+++ e_algorithms/exampleCode/exact-change-dp.py
@@ -0,0 +1,33 @@
+import argparse
+
+def exact_change_dp(x, coins):
+    smallest_coin = min(coins)
+    # If no coin is smaller than x, we can't make change for x.
+    if x < smallest_coin:
+        return None
+
+    fewest_coins = [None for _ in range( x+1 )]
+    fewest_coins[0] = 0 # Don't need any coins to make 0
+    # Start with smallest coin value (all non-zero smaller values are impossible)
+    for target in range(smallest_coin, len(fewest_coins)):
+        # Can we use any of the coins to make the target?
+        options = []
+        for coin in coins:
+            prev = target - coin # Best count without this coin
+            count = fewest_coins[prev] if prev >= 0 else None
+            if not count is None:
+                options.append(count + 1)
+        fewest_coins[target] = min(options) if len(options) > 0 else None
+
+    return fewest_coins[target]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Make change for target value using given coin values.")
+    parser.add_argument("--target", type=int, required=True, help="Target value to make change for")
+    parser.add_argument("--coins", type=int, required=True, nargs="+", help="Coin values to include")
+
+    args = parser.parse_args()
+    target = args.target
+    coin_values = args.coins
+
+    print(exact_change_dp(target, coin_values))
