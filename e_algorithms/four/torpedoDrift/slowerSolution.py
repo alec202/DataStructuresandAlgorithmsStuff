3a58b0729ddd6d4dad5a7f36b8bf5f3afdcb0d14
Alec Mirambeau <alecmirambeau@gmail.com>
2024-03-08 15:24:11 -0500

    just joined output array statements

---
+++ e_algorithms/four/torpedoDrift/slowerSolution.py
@@ -0,0 +1,33 @@
+# This solution was formed after watching this video: https://www.youtube.com/watch?v=dw2nMCxG0ik
+
+def torpedoDrift(nums, target):
+    cache = {0: 1}
+    for i in range(1, target + 1):
+        cache[i] = 0
+    for i in range(1, target + 1):
+        for num in nums:
+            if i - num >= 0:
+                cache[i] += cache[i - num]
+
+    return cache[target]
+
+
+
+
+if __name__ == "__main__":
+    output_arr = []
+    numTestCases = int(input())
+    for i in range(0, numTestCases):
+        numDifferentBricks, _, target = input().partition(" ")
+        numDifferentBricks = int(numDifferentBricks)
+        target = int(target)
+        brickLengths = input().split()
+        brickLengths = [int(brickLength) for brickLength in brickLengths]
+        output_arr.append(f"{torpedoDrift(brickLengths, target) % 1000000009}")
+
+    print("\n".join(output_arr))
+
+
+
+
+
