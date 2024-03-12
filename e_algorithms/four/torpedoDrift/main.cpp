f33563f3cda06e2c26d9ec04ddd976c684303dc2
Alec Mirambeau <alecmirambeau@gmail.com>
2024-03-12 16:08:30 -0400

    Works individually not multiple calls

--- e_algorithms/four/torpedoDrift/main.cpp
+++ e_algorithms/four/torpedoDrift/main.cpp
@@ -10,6 +10,7 @@ int torpedoDrift(std::vector<int> nums, int target){
     cache[0] = 1;
     for( size_t i = 1; i <= target; ++i){
         for (size_t j = 0; j < nums.size(); ++j){
+            // have to debug this line below
             int sub = i - nums[j];
             if (sub >= 0){
                 cache[i] += cache[sub];
@@ -25,16 +26,28 @@ int main(int argc, char** argv){
 //    std::string secondRowData;
     int numBrickTypes;
     int targetLength;
+    int temp;
+    std::vector<int> bricks(0);
     std::cin >> numTestCases;
+    std::vector<int> output(0);
     for(size_t i = 0; i < numTestCases; ++i){
         //getLine should get the entire line entered by user
 //        std::getline(std::cin, firstrowdata);
 //        std::getline(std::cin, secondRowData);
-
+        std::cin >> numBrickTypes >> targetLength;
+        for (size_t j = 0; j < numBrickTypes; ++j){
+            std::cin >> temp;
+            bricks.push_back(temp);
+        }
+        output.push_back(torpedoDrift(bricks, targetLength));
         // We want to turn the input into tokens
 //        char* token = std::strtok(firstrowdata, " ");
-
     }
+
+    for (size_t i = 0; i < output.size(); ++i)
+        std::cout << output[i];
+
+
 
 
 
